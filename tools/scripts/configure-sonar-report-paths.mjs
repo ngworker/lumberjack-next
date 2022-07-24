20ae2e363c8d345cd7dce5ee9023595b7f2ff067
Nacho Vazquez <nachovc1410@gmail.com>
2022-07-24 19:26:48 -0300

    chore: complete sonar configuration

---
+++ tools/scripts/configure-sonar-report-paths.mjs
@@ -0,0 +1,57 @@
+import glob from 'glob';
+import replaceInFile from 'replace-in-file';
+
+const sonarConfigurationPath = 'sonar-project.properties';
+
+async function configureCoverageReportPaths() {
+  const coverageReports = await listCoverageReports();
+  const coverageReportsPattern = coverageReports.join(',');
+  const to = `sonar.javascript.lcov.reportPaths=${coverageReportsPattern}`;
+
+  console.log(to);
+  await replaceInFile({
+    files: sonarConfigurationPath,
+    from: 'sonar.javascript.lcov.reportPaths=<PLACEHOLDER>',
+    to,
+  });
+}
+
+async function configureLintReportPaths() {
+  const lintReports = await listLintReports();
+  const lintReportsPattern = lintReports.join(',');
+  const to = `sonar.typescript.tslint.reportPaths=${lintReportsPattern}`;
+
+  console.log(to);
+  await replaceInFile({
+    files: sonarConfigurationPath,
+    from: 'sonar.typescript.tslint.reportPaths=<PLACEHOLDER>',
+    to,
+  });
+}
+
+function listCoverageReports() {
+  return listFilePaths('coverage/**/lcov.info');
+}
+
+function listFilePaths(pattern) {
+  return new Promise((resolve, reject) => {
+    glob(pattern, (error, filePaths) => {
+      if (error) {
+        reject(error);
+
+        return;
+      }
+
+      resolve(filePaths);
+    });
+  });
+}
+
+function listLintReports() {
+  return listFilePaths('reports/**/lint/report.json');
+}
+
+(async () => {
+  await configureCoverageReportPaths();
+  await configureLintReportPaths();
+})();
