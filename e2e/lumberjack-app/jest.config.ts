1c2133a63a65deed25ef7e60846e01a78646403b
Nacho Vazquez <nachovc1410@gmail.com>
2022-10-09 17:59:16 -0300

    refactor: fix unintentional rename

--- e2e/lumberjack-app/jest.config.ts
+++ e2e/lumberjack-app/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'example',
+  displayName: 'lumberjack-app',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/packages/example',
+  coverageDirectory: '../../coverage/e2e/lumberjack-app',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
