076e55bb982ecd85b5315d0b91ab542544c215a9
Nacho Vazquez <nachovc1410@gmail.com>
2022-10-09 17:53:52 -0300

    refactor: move lumberjack-app to the packages folder

--- packages/example/jest.config.ts
+++ packages/example/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'lumberjack-app',
+  displayName: 'example',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/e2e/lumberjack-app',
+  coverageDirectory: '../../coverage/packages/example',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
